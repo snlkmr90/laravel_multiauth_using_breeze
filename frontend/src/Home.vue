192f97dabcc0cdffbfe93be66c3ab28b92dbabc3
sunil <snlkmr90@gmail.com>
2024-08-11 12:32:49 +0530

    first commit

---
+++ frontend/src/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { createApp } from 'vue'
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+<template>
+    <header>
+        <button @click="count++">{{ count }}</button>
+  
+      <div class="wrapper">
+        <HelloWorld msg="volla" />
+      </div>
+    </header>
+  </template>
+  <script>
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import Home from './Home.vue'
+
+createApp(Home).mount('#app')
